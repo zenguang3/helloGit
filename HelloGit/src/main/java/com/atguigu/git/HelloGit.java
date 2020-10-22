2224daa125118a52c6a00cc9543f2f51c86cfad3
DESKTOP-P03PCRI\00079 <38315132@qq.com>
2020-10-22 20:31:11 +0800

    支干第8行提交

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("第4提交");
         System.out.println("分支提交");
-
+        System.out.println("dev44提交");
+        System.out.println("支干第8行提交");
     }
 }
